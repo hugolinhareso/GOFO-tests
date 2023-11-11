07f5fe6cc104beffc91cee2b35f341f43ead8316
Hugo Linhares <hugo.10000@hotmail.com>
2023-11-10 21:46:16 -0300

    test: add files

---
+++ GOFO Implementation/test/System/PlayGroundScheduleTest.java
@@ -0,0 +1,31 @@
+package System;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlayGroundScheduleTest {
+    
+    public PlayGroundScheduleTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+    
+}
