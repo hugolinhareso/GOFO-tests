ef243b5f00eaca7eccc03d4bb16b52189596dc61
Vítor Martins <65295232+vihmar@users.noreply.github.com>
2023-11-22 19:54:42 -0300

    Tests: Add Playground Tests

--- GOFO Implementation/src/test/java/System/PlaygroundTest.java
+++ GOFO Implementation/src/test/java/System/PlaygroundTest.java
@@ -1,5 +1,82 @@
 package System;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+@RunWith(MockitoJUnitRunner.class)
 public class PlaygroundTest {
 
+
+    @InjectMocks
+    private Playground playground;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testSetAndGetCancellationPeriod() {
+        playground = new Playground();
+        playground.setCancellationPeriod(24);
+        assertEquals(24, playground.getCancellationPeriod());
+    }
+
+    @Test
+    public void testSetAndGetName() {
+        playground = new Playground();
+        playground.setName("Playground 1");
+        assertEquals("Playground 1", playground.getName());
+    }
+
+    @Test
+    public void testSetAndGetOwner() {
+        playground = new Playground();
+        playground.setOwner("Owner Name");
+        assertEquals("Owner Name", playground.getOwner());
+    }
+
+    @Test
+    public void DeveRetornarOkQuandoSolicitadoSlotsDisponiveis() {
+        String inputSimulado = "available";
+        InputStream inputStreamSimulado = new ByteArrayInputStream(inputSimulado.getBytes());
+        System.setIn(inputStreamSimulado);
+        playground = new Playground();
+        playground = loadPlayground();
+        playground.setStatus();
+
+        playground.bookingTheSlot("playerOne", "12", "friday");
+        assertEquals("available", playground.getStatus());
+
+    }
+
+    @Test
+    public void DeveRetornarOkQuandoSolicitadoCancelamentoAgendamento() {
+        String inputSimulado = "available";
+        InputStream inputStreamSimulado = new ByteArrayInputStream(inputSimulado.getBytes());
+        System.setIn(inputStreamSimulado);
+        playground = new Playground();
+        playground = loadPlayground();
+        playground.setStatus();
+
+        playground.cancelBooking("friday", "12");
+        assertEquals("available", playground.getStatus());
+        assertEquals(0, playground.object.getBegin());
+        assertEquals(0, playground.object.getEnd());
+    }
+    
+    public Playground loadPlayground(){
+        playground.setName("Happy Playground");
+        playground.setOwner("Happy Inc");
+
+        return playground;
+    }
 }
